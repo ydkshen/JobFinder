7d7ef3141aab8812212ce5379fae56c2edeea8a6
ydkshen <ydkshen@gmail.com>
2015-01-16 19:58:30 +0000

    root

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "jade");
+app.set("views", __dirname);
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("*", function(req,res) {
+   res.render("index");
+});
+
+app.listen(process.env.PORT, process.env.IP);
